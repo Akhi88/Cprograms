8c190cc8db6c6ef7b9d995f452df8d76e8376174
Akhi88S <39363939+Akhi88S@users.noreply.github.com>
2019-08-31 12:30:32 +0530

    Update Mergesort.c

--- Mergesort.c
+++ Mergesort.c
@@ -1,48 +1,7 @@
 #include <stdio.h>
 #include<conio.h>
 #include<stdlib.h>
-int main()
-{
-    int arr[10]; 			//array initialisation
-    int n1,i;
-    printf("enter size of original array");
-    scanf("%d",&n1);
-    for(i=0;i<n1;i++)
-    {
-    scanf("%d",&arr[i]);  		 //array inputs
-    }
-    mergesort(arr,n1);   		//dividing array into parts
-for(i=0;i<n1;i++)
-    {
-    printf("%d",arr[i]);  		//array after sorting
-    }
 
-    return 0;
-}
-void mergesort(int *arr,int n)
-{
-    int i,j,mid,*L,*R;
-    mid=n/2;
-    if(n<2)
-    {
-    return;    
-    }
-    L = (int*)malloc(mid*sizeof(int));             	//left sub array     
-	R = (int*)malloc((n -mid)*sizeof(int)); 	//rightsubarray
-    for(i=0;i<mid;i++)
-    {
-        L[i]=arr[i];
-    }
-    for(j=mid;j<n;j++)
-    {
-        R[j-mid]=arr[j];
-    } //MergeSort
-    mergesort(L,mid);
-    mergesort(R,n-mid);					
-    merge(arr,L,mid,R,n-mid);				//merge left and right subarrays
-     free(L);
-        free(R);    
-}
 void merge(int *arr,int *L,int leftsize,int *R,int rightsize)
 {
     int nl=leftsize;
@@ -76,4 +35,50 @@ void merge(int *arr,int *L,int leftsize,int *R,int rightsize)
         j++;
         k++;
     }
+}
+
+
+void mergesort(int *arr,int n)
+{
+    int i,j,mid,*L,*R;
+    mid=n/2;
+    if(n<2)
+    {
+    return;    
+    }
+    L = (int*)malloc(mid*sizeof(int));             	//left sub array     
+	R = (int*)malloc((n -mid)*sizeof(int)); 	//rightsubarray
+    for(i=0;i<mid;i++)
+    {
+        L[i]=arr[i];
+    }
+    for(j=mid;j<n;j++)
+    {
+        R[j-mid]=arr[j];
+    } //MergeSort
+    mergesort(L,mid);
+    mergesort(R,n-mid);					
+    merge(arr,L,mid,R,n-mid);				//merge left and right subarrays
+     free(L);
+        free(R);    
+}
+
+
+int main()
+{
+    int arr[10]; 			//array initialisation
+    int n1,i;
+    printf("enter size of original array");
+    scanf("%d",&n1);
+    for(i=0;i<n1;i++)
+    {
+    scanf("%d",&arr[i]);  		 //array inputs
+    }
+    mergesort(arr,n1);   		//dividing array into parts
+for(i=0;i<n1;i++)
+    {
+    printf("%d",arr[i]);  		//array after sorting
+    }
+
+    return 0;
 }
