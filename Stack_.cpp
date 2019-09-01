805e69beda6bea4739a214bc45743a627ea7fb52
Akhi88S <39363939+Akhi88S@users.noreply.github.com>
2019-09-01 10:37:02 +0530

    Update Stack_.cpp

--- Stack_.cpp
+++ Stack_.cpp
@@ -1,13 +1,5 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <iostream>
-#define Max 10
+#define Max 101
 
 using namespace std;
 class Stack
